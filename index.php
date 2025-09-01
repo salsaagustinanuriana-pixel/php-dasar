5673692e2772aad58f01faa52eb02ea1ec2949a7
salsa <salsaagustinanuriana@gmail.com>
2025-09-01 10:43:28 +0700

    buat file index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP DASAR</title>
+</head>
+<body>
+    <?php $nama = "Salsa"?>
+    <h1>Selamat Datang,<?php echo $nama;?>!</h1>
+</body>
+</html>
